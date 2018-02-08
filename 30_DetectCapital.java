54798b5b9455f43aafaba28e6346f40c12a51c36
goginim <david.ingook@gmail.com>
2018-02-08 18:07:48 -0500

    29-30

---
+++ 30_DetectCapital.java
@@ -0,0 +1,39 @@
+// Given a word, you need to judge whether the usage of capitals in it is right or not.
+//
+// We define the usage of capitals in a word to be right when one of the following cases holds:
+//
+// All letters in this word are capitals, like "USA".
+// All letters in this word are not capitals, like "leetcode".
+// Only the first letter in this word is capital if it has more than one letter, like "Google".
+// Otherwise, we define that this word doesn't use capitals in a right way.
+// Example 1:
+// Input: "USA"
+// Output: True
+// Example 2:
+// Input: "FlaG"
+// Output: False
+// Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int length = word.length();
+        int count = 0;
+        boolean first = false;
+
+        for(int i=0; length>i; i++) {
+            if (word.charAt(i) < 97 || word.charAt(i) > 122) {
+                count++;
+                if(i==0)
+                    first = true;
+            }
+        }
+            if(first && count == length)
+                return true;
+            else if(first && count == 1)
+                return true;
+            else if(count == 0)
+                return true;
+            else
+                return false;
+    }
+}
