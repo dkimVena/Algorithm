1032db2fb3cc5a804d5506793bb6321b4ac5b5f7
goginim <david.ingook@gmail.com>
2018-04-27 11:17:24 -0400

    2018-04-27

---
+++ 171.AllPathFromSourceToTarget.js
@@ -0,0 +1,43 @@
+// Given a directed, acyclic graph of N nodes.  Find all possible paths from node 0 to node N-1, and return them in any order.
+
+// The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.  
+// graph[i] is a list of all nodes j for which the edge (i, j) exists.
+
+// Example:
+// Input: [[1,2], [3], [3], []] 
+// Output: [[0,1,3],[0,2,3]] 
+// Explanation: The graph looks like this:
+// 0--->1
+// |    |
+// v    v
+// 2--->3
+// There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+// Note:
+
+// The number of nodes in the graph will be in the range [2, 15].
+// You can print different paths in any order, but you should keep the order of nodes inside one path.
+
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function(graph) {
+    let result = [];
+    let path = [];
+    
+    checkPath(0, path, graph, result);
+    
+    return result;
+};
+
+var checkPath = function(index, path, graph, result) {
+    path = path.concat(index);
+    
+    if(index === graph.length - 1) {
+        result.push(path);
+    } else {
+        graph[index].map(item => {
+           checkPath(item, path, graph, result); 
+        });
+    }
+};
