d63e617819f6baa5b0c9022f3fc7a970a2d0c784
goginim <david.ingook@gmail.com>
2018-06-01 15:36:37 -0400

    2018-06-01

---
+++ 179.MajorityElement.js
@@ -0,0 +1,34 @@
+// Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+// You may assume that the array is non-empty and the majority element always exist in the array.
+
+// Example 1:
+
+// Input: [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: [2,2,1,1,1,2,2]
+// Output: 2
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let size = nums.length;
+    let hashMap = {};
+    let max = 0;
+    let maxKey;
+    
+    for(let i = 0; size > i; i++) {
+        hashMap[nums[i]] = hashMap[nums[i]] + 1 || 1;
+        
+        if(max < hashMap[nums[i]]) {
+            max = hashMap[nums[i]];
+            maxKey = Number(nums[i]);
+        }
+    }
+    
+    return maxKey;
+};
