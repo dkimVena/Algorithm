32d482dea53603b5fcff40b4fdfac1a2571090e4
goginim <david.ingook@gmail.com>
2020-05-12 09:06:40 -0400

    2020-05-12

---
+++ 6-11.1-bitAnd2-bitCharacters.js
@@ -0,0 +1,41 @@
+// We have two special characters. The first character can be represented by one bit 0. The second character can be represented by two bits (10 or 11).
+
+// Now given a string represented by several bits. Return whether the last character must be a one-bit character or not. The given string will always end with a zero.
+
+// Example 1:
+// Input: 
+// bits = [1, 0, 0]
+// Output: True
+// Explanation: 
+// The only way to decode it is two-bit character and one-bit character. So the last character is one-bit character.
+// Example 2:
+// Input: 
+// bits = [1, 1, 1, 0]
+// Output: False
+// Explanation: 
+// The only way to decode it is two-bit character and two-bit character. So the last character is NOT one-bit character.
+// Note:
+
+// 1 <= len(bits) <= 1000.
+// bits[i] is always 0 or 1.
+
+// url: https://leetcode.com/problems/1-bit-and-2-bit-characters/
+
+/**
+ * @param {number[]} bits
+ * @return {boolean}
+ */
+var isOneBitCharacter = function(bits) {
+  const size = bits.length;
+  
+  if(bits[size - 1] !== 0) return false;
+  
+  for(let i=0; i<size; i++) {
+      if(bits[i] === 1) {
+          if(i === size - 2) return false;
+          i++;
+      } 
+  }
+  
+  return true;
+};
